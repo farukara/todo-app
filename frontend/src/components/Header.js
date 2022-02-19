a714673582b562d496bdc409db1ec1afc2dfab11
farukara <69348522+farukara@users.noreply.github.com>
2022-02-19 22:48:14 +0300

    initial commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+    return (
+        <div className="motto">
+            <h1>Todos and some other stuff</h1>
+        </div>
+    )
+}
+
+export default Header;
