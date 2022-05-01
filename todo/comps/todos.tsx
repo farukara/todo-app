1b27f7dd636c68c6ce517b1abf2ef80d5ba541a4
farukara <69348522+farukara@users.noreply.github.com>
2022-05-01 15:52:40 +0300

    added delete all button

--- todo/comps/todos.tsx
+++ todo/comps/todos.tsx
@@ -1,12 +1,18 @@
 import { uid } from 'uid'
-import { useState , useEffect } from "react"
-import { Card, CardContent, List, Box, TextField, Typography } from "@mui/material"
+import { useState , useEffect, useRef } from "react"
+import { Card, CardContent, Modal, List, ListItem, Box, Button, IconButton, TextField, Typography } from "@mui/material"
 import styles from "../styles/Todos.module.css"
 import { TransitionGroup } from 'react-transition-group';
+import ReportProblemIcon from '@mui/icons-material/ReportProblem';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
 
 export default function Todos () {
     const [ text, setText ] = useState("")
-    const [ todos, setTodos ] = useState([])
+    const [ todos, setTodos ] = useState(() => window.localStorage.todos ? localStorage.todos:[])
+    const [ showClearTodosModal, setShowClearTodosModal] = useState(false)
+    const inputRef = useRef()
 
     useEffect(() => {
         if (localStorage.todos) {
@@ -14,9 +20,9 @@ export default function Todos () {
         }
     }, [])
     useEffect(() => {
-        if (todos.length > 0) {
+        // if (todos.length > 0) {
             localStorage.setItem("todos", JSON.stringify(todos))
-        }
+        // }
     }, [todos])
 
     function handleSubmit(e) {
@@ -28,7 +34,6 @@ export default function Todos () {
 
     function markDone(e) {
         setTodos(todos.map(todo => {
-            console.log(e.target.getAttribute("id"))
             if (e.target.innerText === todo.text) {
                 console.log("inside")
                 return {text:todo.text, done:!todo.done}
@@ -37,8 +42,15 @@ export default function Todos () {
             }
         }))
     }
+
+    function deleteTodo(id: string) {
+        const newtodos = todos.filter(todo => todo.id !== id)
+        console.log('newtodos:', newtodos)
+        setTodos(newtodos)
+    }
+
     return (
-        <div>
+        <Box>
             <form
                 onSubmit={handleSubmit}
             >
@@ -48,30 +60,93 @@ export default function Todos () {
                     autoFocus
                     value={text}
                     onChange={(e) => setText(e.target.value)}
+                    ref={inputRef}
                 />
             </form>
 
+            <Button 
+                 color="primary" 
+                 aria-label="upload picture" 
+                 component="span"
+                    className={styles.clearall} 
+                    onClick={() => setShowClearTodosModal(true)}
+                startIcon={<ReportProblemIcon />}
+            >
+                Delete All Todos
+            </Button>
+            <Modal
+              open={showClearTodosModal}
+              onClose={() => setShowClearTodosModal(false)}
+              aria-labelledby="delete all todos"
+              aria-describedby="delete all todos"
+              className={styles.deleteallmodal}
+            >
+              <Box className={styles.modalbox}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Delete all todos?
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Are you sure you want to delete all todos? This action is irreversable and you will lose all of your todos.
+                </Typography>
+                <Button
+                    onClick={() => setShowClearTodosModal(false)}
+                    variant="contained"
+                    sx={{m:1}}
+                >
+                Nah, don't delete my todos
+                </Button>
+                <Button 
+                    onClick={() => {
+                        setTodos([])
+                        setShowClearTodosModal(false)
+                    }}
+                    variant="contained"
+                    startIcon={<ReportProblemIcon />}
+                    sx={{
+                        m:1,
+                        color: "maroon",
+                    }}
+                >
+                Yes I&apos;m sure delete all of them
+                </Button>
+              </Box>
+            </Modal>
+
             <List>
             {todos.map(todo =>(
-                <Card className={styles.card}
-                        key={todo.id}
-                        onClick={markDone}
-                        id={todo.id}
+                <ListItem 
+                    key={todo.id}
+                    onClick={markDone}
                 >
-                    <CardContent>
-                        <Typography
-                                variant="body1"
-                                style= {{
-                                    color: todo.done ? "#555" : ""
-                                }}
-                        >
-                            {todo.text} - {todo.id}
-                        </Typography>
-                    </CardContent>
-                </Card>
+                    <Card className={styles.card}
+                    >
+                            <IconButton 
+                                className={styles.icons}
+                                onClick={() => deleteTodo(todo.id)}
+                                aria-label="delete"
+                            >
+                              <DeleteIcon fontSize="small"/>
+                            </IconButton>
+                            <IconButton 
+                                className={styles.icons}
+                                aria-label="edit"
+                            >
+                              <EditIcon fontSize="small"/>
+                            </IconButton>
+                            <Typography
+                                    variant="body1"
+                                    style= {{
+                                        color: todo.done ? "#555" : "",
+                                        margin: 10,
+                                    }}
+                            >
+                                {todo.text}
+                            </Typography>
+                    </Card>
+                </ListItem>
                 )
             )}
             </List>
-        </div>
+        </Box>
     )
 }
